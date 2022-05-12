9b2ccf58c62da789a7f556bf3ea705b21ebc2d8c
Viktor Blomme <viktor.blomme@fishingcactus.com>
2022-05-12 17:28:03 +0200

    Add validation for light components

--- Source/Private/Validators/MapCheckValidator_LightMobility.cpp
+++ Source/Private/Validators/MapCheckValidator_LightMobility.cpp
@@ -1,5 +1,7 @@
 #include "Validators/MapCheckValidator_LightMobility.h"
 
+#include <Components/LightComponentBase.h>
+#include <Engine/Light.h>
 #include <EngineUtils.h>
 #include <Misc/UObjectToken.h>
 
@@ -12,25 +14,36 @@ void AMapCheckValidator_LightMobility::CheckForErrors()
 
     UClass * actor_class = nullptr;
 
-    for ( TActorIterator< AActor > actor_iterator( GetWorld(), actor_class ); actor_iterator; ++actor_iterator )
+    for ( TObjectIterator< ULightComponentBase > light_component_iterator; light_component_iterator; ++light_component_iterator )
     {
-        const auto * actor = *actor_iterator;
+        const auto * light_component = *light_component_iterator;
 
-        if ( actor->IsEditorOnly() )
+        auto owner_name = light_component->GetOwner()->GetName();
+        auto component_name = GetNameSafe( light_component );
+        const auto light_mobility = light_component->Mobility.GetValue();
+        auto light_mobility_string = UEnum::GetDisplayValueAsText( light_mobility ).ToString();
+
+        if ( light_component->IsA< UDirectionalLightComponent >() )
         {
-            continue;
+            if ( light_mobility != EComponentMobility::Stationary )
+            {
+                MapCheck.Error()
+                    ->AddToken( FUObjectToken::Create( this ) )
+                    ->AddToken( FTextToken::Create( FText::FromString( "Component" ) ) )
+                    ->AddToken( FUObjectToken::Create( light_component ) )
+                    ->AddToken( FTextToken::Create( FText::FromString( FString::Printf( TEXT( "in blueprint %s is %s with light mobility %s instead of Stationary." ), *owner_name, *component_name, *light_mobility_string ) ) ) );
+
+                continue;
+            }
         }
 
-        if ( actor->GetClass() == actor_class )
+        if ( light_mobility != EComponentMobility::Static )
         {
-            const auto * actor_level = actor->GetLevel();
-            const auto actor_level_name = actor_level->GetOuter()->GetName();
-
             MapCheck.Error()
                 ->AddToken( FUObjectToken::Create( this ) )
-                ->AddToken( FTextToken::Create( FText::FromString( "Actor" ) ) )
-                ->AddToken( FUObjectToken::Create( actor ) )
-                ->AddToken( FTextToken::Create( FText::FromString( FString::Printf( TEXT( "in map %s is an actor of type %s or derived and is forbidden in the level." ), *actor_level_name, *GetNameSafe( actor_class ) ) ) ) );
+                ->AddToken( FTextToken::Create( FText::FromString( "Component" ) ) )
+                ->AddToken( FUObjectToken::Create( light_component ) )
+                ->AddToken( FTextToken::Create( FText::FromString( FString::Printf( TEXT( "in blueprint %s is %s with light mobility %s instead of Static." ), *owner_name, *component_name, *light_mobility_string ) ) ) );
         }
     }
 }
