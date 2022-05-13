8441960f6bd91f5f545000e7216bdea3830907d5
Viktor Blomme <viktor.blomme@fishingcactus.com>
2022-05-13 09:59:41 +0200

    Filter lights
    Make sure light actors are valid and they are in the level and the components are valid, otherwise it's not a light that should be checked.
    Simplify code.
    Make sure directional light can either be static or stationary.

--- Source/Private/Validators/MapCheckValidator_LightMobility.cpp
+++ Source/Private/Validators/MapCheckValidator_LightMobility.cpp
@@ -1,5 +1,6 @@
 #include "Validators/MapCheckValidator_LightMobility.h"
 
+#include <Components/DirectionalLightComponent.h>
 #include <Components/LightComponentBase.h>
 #include <Engine/Light.h>
 #include <EngineUtils.h>
@@ -12,38 +13,55 @@ void AMapCheckValidator_LightMobility::CheckForErrors()
 
     FMessageLog MapCheck( "MapCheck" );
 
-    UClass * actor_class = nullptr;
-
     for ( TObjectIterator< ULightComponentBase > light_component_iterator; light_component_iterator; ++light_component_iterator )
     {
         const auto * light_component = *light_component_iterator;
 
-        auto owner_name = light_component->GetOwner()->GetName();
-        auto component_name = GetNameSafe( light_component );
+        if ( !IsValid( light_component ) )
+        {
+            continue;
+        }
+
+        const auto * light_actor = light_component->GetOwner();
+
+        if ( light_actor == nullptr )
+        {
+            continue;
+        }
+
+        const auto light_level = light_actor->GetLevel();
+
+        if ( light_level == nullptr )
+        {
+            continue;
+        }
+
         const auto light_mobility = light_component->Mobility.GetValue();
-        auto light_mobility_string = UEnum::GetDisplayValueAsText( light_mobility ).ToString();
+
+        auto has_expected_mobility = light_mobility == EComponentMobility::Static;
+        auto expected_mobility_string = FString( "Static" );
 
         if ( light_component->IsA< UDirectionalLightComponent >() )
         {
-            if ( light_mobility != EComponentMobility::Stationary )
-            {
-                MapCheck.Error()
-                    ->AddToken( FUObjectToken::Create( this ) )
-                    ->AddToken( FTextToken::Create( FText::FromString( "Component" ) ) )
-                    ->AddToken( FUObjectToken::Create( light_component ) )
-                    ->AddToken( FTextToken::Create( FText::FromString( FString::Printf( TEXT( "in blueprint %s is %s with light mobility %s instead of Stationary." ), *owner_name, *component_name, *light_mobility_string ) ) ) );
-
-                continue;
-            }
+            has_expected_mobility = light_mobility == EComponentMobility::Static || EComponentMobility::Stationary;
+            expected_mobility_string = FString( "Static or Stationary" );
         }
 
-        if ( light_mobility != EComponentMobility::Static )
+        if ( !has_expected_mobility )
         {
+            const auto component_name = GetNameSafe( light_component );
+            const auto light_mobility_string = UEnum::GetDisplayValueAsText( light_mobility ).ToString();
+            auto light_level_name = light_level->GetOuter()->GetName();
+
             MapCheck.Error()
                 ->AddToken( FUObjectToken::Create( this ) )
-                ->AddToken( FTextToken::Create( FText::FromString( "Component" ) ) )
-                ->AddToken( FUObjectToken::Create( light_component ) )
-                ->AddToken( FTextToken::Create( FText::FromString( FString::Printf( TEXT( "in blueprint %s is %s with light mobility %s instead of Static." ), *owner_name, *component_name, *light_mobility_string ) ) ) );
+                ->AddToken( FTextToken::Create( FText::FromString( "Actor" ) ) )
+                ->AddToken( FUObjectToken::Create( light_actor ) )
+                ->AddToken( FTextToken::Create( FText::FromString( FString::Printf( TEXT( "in map %s has component %s with light mobility %s instead of %s." ),
+                    *light_level_name,
+                    *component_name,
+                    *light_mobility_string,
+                    *expected_mobility_string ) ) ) );
         }
     }
 }
